22569ac665a645b3998f67446fc6f0385202acf5
Snehashis Dasgupta <snehashis.jisu.cse@gmail.com>
2024-02-25 20:03:59 +0500

    Created message.model and conversation.model, send-messages endpoint and protect-route-middleware added.

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -24,7 +24,8 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: "",
     },
-})
+    //createdAt, updatedAt
+}, { timestamps: true });
 
 const User = mongoose.model("User", userSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import dotenv from "dotenv";
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectTOMongoDB.js";
 
 const app = express();
@@ -10,8 +12,10 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json());//to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
 
 // app.get("/", (req, res)=>{
 //     res.send("Hello World");
