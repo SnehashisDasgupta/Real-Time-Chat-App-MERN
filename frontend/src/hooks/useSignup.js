78cda0ef4efc8305d27c45a54fb729be60a63e1a
Snehashis Dasgupta <snehashis.jisu.cse@gmail.com>
2024-02-27 14:58:31 +0500

    Created AuthContext and data stored in localStorage. [Once login, user will directly go to home page]

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,8 +1,11 @@
 import { useState } from "react"
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
+
 
 const useSignup = () => {
   const [loading,setLoading] = useState(false);
+  const {setAuthUser } = useAuthContext();
 
   const signup = async({fullName, username, password, confirmPassword, gender}) => {
     const success = handleInputErrors({fullName, username, password, confirmPassword, gender});
@@ -18,7 +21,12 @@ const useSignup = () => {
         })
 
         const data = await res.json();
-        console.log(data);
+        if (data.error){
+            throw new Error(data.error)
+        }
+        // localStorage
+        localStorage.setItem("chat-user", JSON.stringify(data));
+        setAuthUser(data);
 
     } catch (error) {
         toast.error(error.message);
