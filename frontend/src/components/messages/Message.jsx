97c46265102c8bcec36b6f00d19833a74c748570
Snehashis Dasgupta <snehashis.jisu.cse@gmail.com>
2024-03-01 14:27:58 +0500

    Added get message functionality, smooth scrolling, loading skeleton

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,15 +1,26 @@
-
-const Message = () => {
+import { useAuthContext } from '../../context/AuthContext';
+import { extractTime } from '../../utils/extractTime';
+import useConversation from '../../zustand/useConversation';
+ 
+const Message = ({message}) => {
+  const {authUser} = useAuthContext();
+  const { selectedConversation } = useConversation();
+  const fromMe = message.senderId === authUser._id;
+  const chatClassName = fromMe ? 'chat-end' : 'chat-start';
+  const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? 'bg-blue-700' : '';
+  const formatedTime = extractTime(message.createdAt);
+  
   return (
-    <div className="chat chat-end">
+    <div className={`chat ${chatClassName}`}>
         <div className="chat-image avatar">
             <div className="w-10 rounded-full">
-                <img src="" alt="avatar" />
+                <img src={profilePic} alt="avatar" />
             </div>
         </div>
 
-        <div className={`chat-bubble text-white bg-blue-500`}>Hi! WHat is Up?</div>
-        <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">23:43</div>
+        <div className={`chat-bubble text-white ${bubbleBgColor}`}>{message.message}</div>
+        <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">{formatedTime}</div>
     </div>
   )
 }
