adc40187d5458e615cf39a89e67f0bda1a01e263
Snehashis Dasgupta <snehashis.jisu.cse@gmail.com>
2024-03-06 20:38:24 +0500

    Changed littlebit code of server.js

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,6 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
-
-dotenv.config();
-
 app.use(express.json());//to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
@@ -24,11 +20,18 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+// ---------------- Deployment---------------------
+const __dirname = path.resolve();
+
+dotenv.config();
+
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-})
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
+// -----------------------------------------------
+
 
 
 server.listen(PORT, ()=> {
