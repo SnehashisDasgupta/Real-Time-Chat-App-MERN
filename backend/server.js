cfd3188421142b7852d7f82efea95d0177340532
Snehashis Dasgupta <snehashis.jisu.cse@gmail.com>
2024-06-12 16:32:47 +0500

    .

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,10 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
+dotenv.config();
+
 app.use(express.json());//to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
@@ -21,17 +25,13 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
 // ---------------- Deployment---------------------
-const __dirname = path.resolve();
-
-dotenv.config();
-
+// express.static is used to serve static files like html, css, js
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 // -----------------------------------------------
-
 
 
 server.listen(PORT, ()=> {
